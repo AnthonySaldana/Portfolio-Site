836c8e6fcdeef1a5610cf9b3c57f0cf8f0220b63
Anthony saldana <anthonysaldana03@gmail.com>
2014-09-06 19:03:11 -0700

    Online Portfolio
    An online portfolio i am developing. Will be showcasing all work I have
    done.

---
+++ index.php
@@ -0,0 +1,80 @@
+<head>
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<title>Portfolio - Web Developer</title>
+</head>
+<body>
+<div class="container portfolio">
+	<div class="jumbotron">
+		<center><h1 style="border-bottom:1px solid;">Anthony Saldana<br/></h1>
+		<h2>Web Developer</h2>
+		<a href="mailto:anthonysaldana@leafyweb.com">anthonysaldana@leafyweb.com</a></center>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<h2 style="border-bottom:1px solid;">About Me</h2>
+			<p class="h4">I'm Anthony Saldana, a Los Angeles based Web Developer. I passionately build code
+			to create clean and user-friendly websites. My current technologies of choice are php and 
+			MySql for my back-end work and simply HTML, CSS with bootstrap, javascript and jquery for
+			my front-end work. With skill as an engineer as well as a designer, I offer a view on technology
+			that few possess.</p>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-6">
+			<h3>Technical Skill</h2>
+			<ul class="h4">
+				<li>PHP</li>
+				<li>SQL, MySql</li>
+				<li>Javascript, Jquery</li>
+				<li>CSS, Bootstrap, HTML</li>
+			</ul>
+		</div>
+		
+		<div class="col-md-6">
+			<h3>Professional Skill</h2>
+			<ul class="h4">
+				<li>Work in teams of various abilities</li>
+				<li>Leadership of people with different talent</li>
+				<li>Self-Motivated</li>
+			</ul>
+		</div>
+	</div><!-- skills row -->
+	
+	<div class="row">
+		<div class="col-md-12">
+			<h2 style="border-bottom:1px solid;">Experience</h2>
+			<ul>
+				<h3>Hypewell-Intern<br/><p class="h4">Web Developer | August - Current</p></h3>
+				<p class="h4">Work with designer and marketing staff to translate client request into 
+				functional sites. Held responsible with communicating to clients to ensure 
+				sites follow their vision. As the Sole developer I am tasked with developing 
+				and maintaining client sites using PHP, HTML, CSS, JavaScript, and Wordpress.
+				Every day I am faced with adding, removing, or fixing a feature in a clients
+				website using any of the technologies </p>
+				<li>Work with clients in restaurant and electronics industry.</li>
+				<li>Developed Ovivo.tv site from scratch using full stack technologies.</li>
+				<li>Tweak Wordpress themes using php and javascript to fit clients needs.</li>
+			</ul>
+			
+			<ul>
+				<h3>Coding Sailor.com <p class="h4">-In Progress | Founder/Developer</p></h3>
+				<p>A personal blog with a focus on tech and life. This blog
+				is currently in development and is utilizing full stack technologies.
+				Work on this blog has exposed me to server environment, sql, and php.
+				Although in progress, it will contain a few important features which
+				involve data manipulation from a database using php.</p>
+				<li>Data driven blog with a custom built CMS, membership and profile system using
+				PHP and mysql.</li>
+				<li>Will be implenting a commenting system</li>
+				<li>Technologies used: PHP (with some oop), MySql, HTML, CSS with bootstrap</li>
+			</ul>
+			
+		</div>
+	</div>
+</div>
+</body>
+<?php
+echo"portfolio under construction!";
+?>
