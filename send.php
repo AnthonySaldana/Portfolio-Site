b0b463e74c803aa20e4fa8d6c10bbaa0fb20aeb4
Anthony saldana <anthonywebsol@gmail.com>
2016-06-14 23:39:47 -0700

    catching site up

--- send.php
+++ send.php
@@ -1,37 +1,11 @@
 <?php
-
-if(isset ($_POST['email']) and isset($_POST['text']) and !empty($_POST['email']) and !empty($_POST['text']) )
-{
-    $email = $_POST['email'];
-    $text = $_POST['text'];
-    $message = "User email:". $email . "<br/> message:" . $text . ".";
-    $receive = "anthonysaldana@leafyweb.com";
-    $subject = "Message from your portfolio";
-    if( mail($receive, $subject, $message)) 
-	 {
-	    $pass = "Message has been delivered"; 
-		ob_start();
-	    echo "<script type='text/javascript'>alert('$pass');
-		window.location.replace('index.html');</script>";
-	 }
-    else 
-	 {
-	    $error="error sending message"; 
-		ob_start();
-	    echo "<script type='text/javascript'>alert('$error');
-				window.location.replace('index.html#contact');</script>";
-	 }
-}
-else
-{
-	
-	$error = "Email or Message not set. Please go fill out both forms.";
-	ob_start();
-	echo "<script type='text/javascript'> alert('$error');
-			window.location.replace('index.html#contact');</script>";
-}
-ob_flush();
-
-
-
+require_once 'mailclass.php';
+$contact = new contact;
+$contact -> validateemail($_POST['email']);
+//$contact -> validatephone($_POST['phone']);
+//$contact -> validatename($_POST['name']);
+$contact -> validatemessage($_POST['text']);
+$subject = "Message from Portfolio"; // change to desired subject line
+$email = 'anthonywebsol@gmail.com'; //change to desired email
+$contact ->sendmail($subject , $email);
 ?>
