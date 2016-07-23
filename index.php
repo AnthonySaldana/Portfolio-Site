3b66a13e764dcd1260f09f6557441f74cc1eba79
Anthony saldana <anthonywebsol@gmail.com>
2016-07-23 00:30:02 -0700

    Add Tracking Script

--- index.php
+++ index.php
@@ -23,6 +23,16 @@
     
     <script src="assets/js/jquery.min.js"></script>
 	<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-81186522-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	<style>
 		.prettyprint{
 			font-size:.85em;
